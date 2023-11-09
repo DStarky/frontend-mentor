346ecf8d54a2fb1bf4d3c6a14fb768b1a3536211
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-09 12:44:12 +0500

    first commit

---
+++ 2-qr-code/src/App.tsx
@@ -0,0 +1,11 @@
+import Card from './components/Card';
+
+const App = () => {
+	return (
+		<main className='h-screen bg-bg flex justify-center items-center'>
+			<Card />
+
+		</main>
+	);
+};
+export default App;
