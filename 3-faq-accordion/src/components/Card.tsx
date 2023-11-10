9370f4780b9c824988b904fd3b413078210948ef
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-10 10:00:36 +0500

    already done

--- 3-faq-accordion/src/components/Card.tsx
+++ 3-faq-accordion/src/components/Card.tsx
@@ -6,6 +6,7 @@ import { Question } from 'src/types';
 import imageMobile from 'src/assets/images/illustration-woman-online-mobile.svg';
 import imageDesktop from 'src/assets/images/illustration-woman-online-desktop.svg';
 import shadowMobile from 'src/assets/images/bg-pattern-mobile.svg';
+import patternDesktop from 'src/assets/images/bg-pattern-desktop.svg';
 
 const Card = () => {
 	const [data, setData] = useState<Question[]>(questions);
@@ -22,8 +23,8 @@ const Card = () => {
 	};
 
 	return (
-		<div className='bg-white pt-32 px-6 pb-7 rounded-3xl shadow-main relative'>
-			<div>
+		<div className='bg-white pt-32 px-6 pb-7 rounded-3xl shadow-main relative lg:pt-[65px] lg:px-[95px] lg:pb-[83px] lg:w-[920px] lg:flex lg:flex-col lg:overflow-hidden'>
+			<div className='lg:hidden'>
 				<img
 					srcSet={`${imageMobile} 320w, ${imageDesktop} 768w`}
 					src={imageMobile}
@@ -38,16 +39,30 @@ const Card = () => {
 					className='absolute left-1/2 -translate-x-1/2 top-0'
 				/>
 			</div>
-			<h2 className='text-xl color-bold mb-10 text-center'>FAQ</h2>
-			<ul>
-				{data.map(question => (
-					<FAQ
-						{...question}
-						key={question.id}
-						openHandler={openAnswerHandler}
-					/>
-				))}
-			</ul>
+			<div className='max-lg:hidden'>
+				<img
+					src={imageDesktop}
+					alt='illustration'
+					className='absolute -left-20 z-10'
+				/>
+				<img
+					src={patternDesktop}
+					alt='illustration'
+					className='absolute -left-6 top-0'
+				/>
+			</div>
+			<div className='lg:ml-auto'>
+				<h2 className='text-xl color-bold mb-10 text-center lg:text-left'>FAQ</h2>
+				<ul className=''>
+					{data.map(question => (
+						<FAQ
+							{...question}
+							key={question.id}
+							openHandler={openAnswerHandler}
+						/>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
